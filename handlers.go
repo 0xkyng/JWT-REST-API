f958904ef51e689f0d7287ac9edff07d0d6530aa
Isaac Wanger <teryima127@gmail.com>
2022-12-28 18:54:13 +0100

    refresh

--- handlers.go
+++ handlers.go
@@ -116,5 +116,65 @@ func Home(w http.ResponseWriter, r *http.Request) {
 }
 
 func Refresh(w http.ResponseWriter, r *http.Request) {
+	cookie, err := r.Cookie("token")
+	if err != nil {
+		if err == http.ErrNoCookie {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	tokenStr := cookie.Value
+
+	claims := &Claims{}
+	
+	tkn, err := jwt.ParseWithClaims(tokenStr, claims,
+		func(t *jwt.Token) (interface{}, error) {
+			return jwtKey, nil
+		})
+
+	if err != nil {
+		if err == jwt.ErrSignatureInvalid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	if !tkn.Valid {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	// Check token expiration time
+	// Refresh at 30 seconds
+	if time.Unix(claims.ExpiresAt, 0).Sub(time.Now()) > 30 * time.Second {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Create a new token after refreshing
+	expirationTime := time.Now().Add(time.Minute * 5)
+
+	claims.ExpiresAt = expirationTime.Unix()
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Set cookie
+	http.SetCookie(w,
+		&http.Cookie{
+			Name: "refresh_token",
+			Value: tokenString,
+			Expires: expirationTime,
+		})
 
 }
