6574a5de54182e3a2ce54c8a87af691a6cc26472
Isaac Wanger <teryima127@gmail.com>
2022-12-28 14:33:53 +0100

    login

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/codekyng/JWT-REST-API.git
 
 go 1.19
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
--- handlers.go
+++ handlers.go
@@ -1,6 +1,12 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
 
 var jwtKey =[]byte("secret_key")
 
@@ -14,7 +20,55 @@ type Credentials struct {
 	Password string		`json:"password"`
 }
 
+type Claims struct {
+	Username string		`json:"username"`
+	jwt.StandardClaims
+}
+
 func Login(w http.ResponseWriter, r *http.Request) {
+	var credentials Credentials
+	// Get data from the request body & decode into credentials
+	err := json.NewDecoder(r.Body).Decode(&credentials)
+	// Handle error
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Check if the password tallies with the one in credentials
+	expectedPassword, ok := Users[credentials.Username]
+	
+	if !ok || expectedPassword != credentials.Password {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
+	}
+
+	// Create token expiration time (5 minutes)
+	expirationTime := time.Now().Add(time.Minute * 5)
+
+	// Create object for claims
+	claims := &Claims{
+		Username: credentials.Username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+	// Create token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256,claims)
+	tokenString, err := token.SignedString(jwtKey) // token signing string
+	// Handle error
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// Set cookies
+	http.SetCookie(w,
+		&http.Cookie{
+			Name:	"token",
+			Value:	tokenString,
+			Expires: expirationTime,
+		})
 
 }
 
